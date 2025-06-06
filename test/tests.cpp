85cd0303b102961edcf7ae8efbd311e9d772635a
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-07 00:31:45 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,21 +5,19 @@
 
 #include "tree.h"
 TEST(PMTreeTest, BasicFunctionality) {
-    std::vector<char> elems = {'A', 'B', 'C'};
-    PMTree tree(elems);
-    auto all_paths = tree.getAllPerms();
-    ASSERT_EQ(all_paths.size(), 6);
-    std::set<std::vector<char>> unique_paths(all_paths.begin(), all_paths.end());
-    ASSERT_EQ(unique_paths.size(), 6);
-    for (const auto& p : all_paths) {
-        ASSERT_EQ(p.size(), 3);
+    std::vector<char> elements = {'A', 'B', 'C'};
+    PMTree tree(elements);
+    auto permutations = tree.getAllPerms();
+    ASSERT_EQ(permutations.size(), 6) <<;
+    std::set<std::vector<char>> unique_perms(permutations.begin(), permutations.end());
+    ASSERT_EQ(unique_perms.size(), 6) <<;
+    for (const auto& perm : permutations) {
+        ASSERT_EQ(perm.size(), 3);
+        ASSERT_NE(std::find(perm.begin(), perm.end(), 'A'), perm.end());
+        ASSERT_NE(std::find(perm.begin(), perm.end(), 'B'), perm.end());
+        ASSERT_NE(std::find(perm.begin(), perm.end(), 'C'), perm.end());
     }
-    std::vector<char> path1 = tree.getPerm1(1);
-    ASSERT_EQ(path1.size(), 3);
-    std::vector<char> path2 = tree.getPerm2(2);
-    ASSERT_EQ(path2.size(), 3);
 }
-
 TEST(PMTreeTest, EdgeCases) {
     PMTree empty_tree({});
     ASSERT_TRUE(empty_tree.getAllPerms().empty());
