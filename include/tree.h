e1aff9d38389f4a30734d8990d32d3f11bea315b
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-06 23:26:01 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -8,25 +8,23 @@ class PMTree {
     explicit PMTree(const std::vector<char>& elems);
     ~PMTree();
     std::vector<std::vector<char>> getAllPerms() const;
-    std::vector<char> getPerm1(int n) const;
-    std::vector<char> getPerm2(int n) const;
+    std::vector<char> getPerm1(int num) const;
+    std::vector<char> getPerm2(int num) const;
 
  private:
     struct Node {
-      char value;
-      std::vector<Node*> children;
-      explicit Node(char val);
-      ~Node();
+        char value;
+        std::vector<Node*> children;
+        explicit Node(char val);
+        ~Node();
     };
-
     Node* root;
-    void generatePerms(Node* node,
-std::vector<char>& elems);
-    bool getPerm2Helper(const Node* node,
-int& count, int target,
-std::vector<char>& path) const;
+    void generatePerms(Node* node, std::vector<char>& elems, 
+                      std::vector<std::vector<char>>& path) const;
+    bool getPerm1Helper(Node* node, int& count, int target,
+                       std::vector<char>& path) const;
+    bool getPerm2Helper(Node* node, int& count, int target,
+                       std::vector<char>& path) const;
     void clear(Node* node);
 };
-std::vector<char> getPerm1(PMTree& tree, int num);
-std::vector<char> getPerm2(PMTree& tree, int num);
 #endif  // INCLUDE_TREE_H_
