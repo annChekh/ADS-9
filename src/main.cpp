0ea0391eaf8ff0e626e7689163c51f7788688581
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-06 23:28:06 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,31 @@
 #include <vector>
 #include <chrono>
 #include "tree.h"
-
 int main() {
-  std::vector<char> elems = {'A', 'B', 'C'};
-  PMTree pTree(elems);
-  std::vector<std::vector<char>> res = pTree.getAllPerms();
-  for (const auto& perm : res) {
-    for (char c : perm) {
-      std::cout << c;
+    std::vector<char> elems = {'A', 'B', 'C'};
+    PMTree pTree(elems);
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::vector<std::vector<char>> path = pTree.getAllPerms();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    for (const auto& perm : path) {
+        for (char c : perm) {
+            std::cout << c;
+        }
+        std::cout << '\n';
     }
-    std::cout << std::endl;
-  }
-  std::vector<char> perm1 = pTree.getPerm1(1);
-  std::cout << "Permutation 1: ";
-  for (char c : perm1) {
-      std::cout << c;
-  }
-  std::cout << '\n';
-  std::vector<char> perm2 = pTree.getPerm2(2);
-  std::cout << "Permutation  2: ";
-  for (char c : perm2) {
-      std::cout << c;
-  }
-  std::cout << '\n';
-  auto t1 = std::chrono::high_resolution_clock::now();
-  auto t2 = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed = t2 - t1;
-  std::cout << "Time: " << elapsed.count() << " s\n";
-  return 0;
+    std::vector<char> perm1 = pTree.getPerm1(1);
+    std::cout << "Permutation 1: ";
+    for (char c : perm1) {
+        std::cout << c;
+    }
+    std::cout << '\n';
+    std::vector<char> perm2 = pTree.getPerm2(2);
+    std::cout << "Permutation 2: ";
+    for (char c : perm2) {
+        std::cout << c;
+    }
+    std::cout << '\n';
+    std::chrono::duration<double> elapsed = t2 - t1;
+    std::cout << "Time: " << elapsed.count() << " s\n";
+    return 0;
 }
