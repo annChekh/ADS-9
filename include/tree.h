8ad353421e022135ec978195155792eb1a11fe9d
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-06 22:17:49 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,33 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+class PMTree {
+public:
+    PMTree(const std::vector<char>& elems);
+    ~PMTree();
+    std::vector<std::vector<char>> getAllPerms() const;
+    std::vector<char> getPerm1(int n) const;
+    std::vector<char> getPerm2(int n) const;
 
+private:
+    struct Node {
+        char value;
+        std::vector<Node*> children;
+        Node(char val);
+        ~Node();
+    };
+
+    Node* root;
+    void generatePerms(Node* node,
+std::vector<char>& curr,
+std::vector<std::vector<char>>& res) const;
+    void getPerm1Helper(const Node* node,
+int& cnt, int n,
+std::vector<char>& res) const;
+    void getPerm2Helper(const Node* node,
+int& cnt, int n,
+std::vector<char>& res) const;
+    void clear(Node* node);
+};    
 #endif  // INCLUDE_TREE_H_
