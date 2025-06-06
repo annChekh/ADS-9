15d6a926f38b0f19ee3abbd5a35f52b3edaa7e78
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-07 01:09:13 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,26 +2,31 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 #include <vector>
-#include <memory>
+
+class TreeNode {
+ public:
+    char label;
+    std::vector<TreeNode*> subnodes;
+
+    explicit TreeNode(char l);
+    ~TreeNode();
+};
 
 class PMTree {
- public:
-    explicit PMTree(const std::vector<char>& elems);
-    ~PMTree();
-    std::vector<std::vector<char>> getAllPerms() const;
-    std::vector<char> getPerm1(int num) const;
-    std::vector<char> getPerm2(int num) const;
-
  private:
-    struct Node {
-        char value;
-        std::vector<std::unique_ptr<Node>> children;
-        explicit Node(char val);
-    };
-    std::unique_ptr<Node> root;
-    void generatePerms(Node* node, std::vector<char>& elems,
-                     std::vector<std::vector<char>>& path) const;
-    bool getPermHelper(Node* node, int& count, int num,
-                     std::vector<char>& path, bool usePerm1) const;
+    TreeNode* base;
+    void expand(TreeNode* parent, std::vector<char> rest);
+    void traverse(TreeNode* curr,
+                  std::vector<char>& route,
+                  std::vector<std::vector<char>>& results);
+    std::vector<char> fetch_by_order(TreeNode* curr, int& idx, int goal);
+
+ public:
+    explicit PMTree(const std::vector<char>& chars);
+    ~PMTree();
+    friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+    friend std::vector<char> getPerm1(PMTree& tree, int num);
+    friend std::vector<char> getPerm2(PMTree& tree, int num);
 };
+
 #endif  // INCLUDE_TREE_H_
